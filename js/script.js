bf42bf4e437830629a567eb8c520742dab25ec4a
Andrea <grassiandrea95@gmail.com>
2020-12-01 17:32:45 +0100

    commenti

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 var app=new Vue({
   el:'#app',
   data:{
-    img:'spotify.png',
-    albums:[]
+    img:'spotify.png',  //logo
+    albums:[]   //array che contiene tutti i dischi
   },
   mounted:function () {
+    //cihamo l'api
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((risposta) => {
       this.albums=risposta.data.response;
