ea407e2d1e5462d2e21f2dd6e0caab44a30950e1
Andrea <grassiandrea95@gmail.com>
2020-12-01 23:46:13 +0100

    update div genere-musicale

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -3,11 +3,13 @@ var app=new Vue({
   data:{
     img:'../spotify.png',  //logo
     albums:[],   //array che contiene tutti i dischi
-    generi:['---'] //array che contiene i generi musicali
+    generi:['Tutti i generi'], //array che contiene i generi musicali
+    attiva:false,
+    indiceAttivo:0,
+    tendina:'none'
   },
-  
+
   mounted:function () {
-    //cihamo l'api
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((risposta) => {
       this.albums=risposta.data.response;
       this.albums.forEach((item, i) => {
@@ -19,14 +21,25 @@ var app=new Vue({
   },
 
   methods:{
+
+    // funzione per il dropdown
+    dropdown:function () {
+      if (this.tendina=='none') {
+        this.tendina='block';
+      }else {
+        this.tendina='none';
+      }
+    },
+
     //funzione per scegliere il genere
     scegliGenere:function (i) {
       this.albums=[];
+      this.indiceAttivo=i;
       axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((risposta) => {
         risposta.data.response.forEach((item) => {
           if (item.genre==this.generi[i]) {
             this.albums.push(item);
-          }else if(this.generi[i]=='---'){
+          }else if(this.generi[i]=='Tutti i generi'){
             this.albums.push(item);
           }
         });
